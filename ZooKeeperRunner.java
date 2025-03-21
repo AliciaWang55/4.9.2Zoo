acd18782734fa39ae19be28de582a8cb28f5cf4a
AngelaXu77 <xu9836@mydusd.org>
2025-03-21 13:23:02 -0700

    Update ZooKeeperRunner.java

--- ZooKeeperRunner.java
+++ ZooKeeperRunner.java
@@ -5,6 +5,20 @@ public class ZooKeeperRunner
 {
   public static void main(String[] args)
   { 
+    System.out.println("My Felines");
+    Lion myLion = new Lion();
+    myLion.setName("Romeo");
+    myLion.growl();
+    myLion.roar();
+    myLion.huntInPack();
+
+    Tiger myTiger = new Tiger();
+    myTiger.setName("Tybalt");
+    myTiger.growl();
+    myTiger.roar();
+    myTiger.huntAlone();
+    myTiger.swim();
+    
        
   }
 }
