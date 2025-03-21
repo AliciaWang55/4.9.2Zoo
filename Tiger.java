a7b445fd9802e88a86fb2ae5192efd0627e7d23e
AliciaWang55 <131703090+AliciaWang55@users.noreply.github.com>
2025-03-21 13:23:47 -0700

    Create Tiger.java

---
+++ Tiger.java
@@ -0,0 +1,12 @@
+public class Tiger extends Feline
+{
+  public void huntAlone()
+  {
+    System.out.println(getName() + ": hunt alone");
+  }
+
+  public void swim()
+  {
+    System.out.println(getName() + ": blurblurblurb");
+  }
+ }
